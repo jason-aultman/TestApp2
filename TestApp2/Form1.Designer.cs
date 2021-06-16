8c0ff6b895d88a5041cb7147bfc49f5318009d89
Jason Aultman <jason.aultman@gmail.com>
2021-06-15 21:58:17 -0400

    Add project files.

---
+++ TestApp2/Form1.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace TestApp2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.rtbOne = new System.Windows.Forms.RichTextBox();
+            this.btnActivateTextBoxText = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // rtbOne
+            // 
+            this.rtbOne.Location = new System.Drawing.Point(90, 51);
+            this.rtbOne.Name = "rtbOne";
+            this.rtbOne.Size = new System.Drawing.Size(403, 372);
+            this.rtbOne.TabIndex = 0;
+            this.rtbOne.Text = "Hello";
+            // 
+            // btnActivateTextBoxText
+            // 
+            this.btnActivateTextBoxText.Location = new System.Drawing.Point(579, 71);
+            this.btnActivateTextBoxText.Name = "btnActivateTextBoxText";
+            this.btnActivateTextBoxText.Size = new System.Drawing.Size(75, 23);
+            this.btnActivateTextBoxText.TabIndex = 1;
+            this.btnActivateTextBoxText.Text = "button1";
+            this.btnActivateTextBoxText.UseVisualStyleBackColor = true;
+            this.btnActivateTextBoxText.Click += new System.EventHandler(this.btnActivateTextBoxText_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnActivateTextBoxText);
+            this.Controls.Add(this.rtbOne);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox rtbOne;
+        private System.Windows.Forms.Button btnActivateTextBoxText;
+    }
+}
+
---
+++ TestApp2/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TestApp2
+{
+    public partial class Form1 : Form
+    {
+        public Form1(string[] args)
+        {
+            InitializeComponent();
+            ProcessArgs(args);
+            
+        }
+
+        private void btnActivateTextBoxText_Click(object sender, EventArgs e)
+        {
+            UpdateTextBoxText("Textbox Updated");
+        }
+
+        private void UpdateTextBoxText(string text)
+        {
+            this.rtbOne.Text = text;
+        }
+        private void ProcessArgs(string[] args)
+        {
+            if (args.Length > 0)
+            {
+                if(args.Contains("autorun"))
+                {
+                UpdateTextBoxText(args[0]);
+                }
+                else if (args.Contains("fail"))
+                {
+                    UpdateTextBoxText("Load Failed");
+                }
+
+            }
+        }
+    }
+}
